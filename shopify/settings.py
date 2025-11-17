17a3ef1c45ea5bedaba16903c0bc8eb2efd1fc11
Sunday Othniel <Sundayothniel@outlook.com>
2025-11-17 09:48:48 +0100

    Change email backend to Anymail with Resend API

--- shopify/settings.py
+++ shopify/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: v.split(','))
 
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')  
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+
+ANYMAIL = {
+    "RESEND_API_KEY": config("RESEND_API_KEY"),
+}
+
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 EMAIL_TIMEOUT = 30
+
 
 SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT')
 
